b3a33d1e9c005515e2e590d4920928186a46ffea
Rejuwane <rezwanrian1@gmail.com>
2023-09-15 23:31:20 +0600

    second edited

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import { useEffect} from 'react';
 import './Home.css'
 import { useState} from 'react';
 import Cart from '../Cart/Cart';
+import {  AiOutlineCalendar, AiOutlineStrikethrough} from 'react-icons/ai';
 
 const Home = () => {
 const [courses, setCourses] = useState([]);
@@ -20,6 +21,7 @@ const [cost, setCost] = useState(0)
         const isExist = selectedCourse.find((item)=> item.course_title == course.course_title);
 
         let count = course.credit;
+        
         if(isExist){
             return alert('already booked')
         }
@@ -28,9 +30,16 @@ const [cost, setCost] = useState(0)
                 count =count + item.credit;      
             });
             const totalRemaining = 20 - count;
+            if(count >20){
+           return alert('Total Credit Hour 20');
+        }
+           else{
             setCost(count);
             setRemaining(totalRemaining)
-            setSelectedCourse([...selectedCourse, course])
+            setSelectedCourse([...selectedCourse, course]);
+           }
+            
+           
         }
 
        
@@ -48,12 +57,12 @@ console.log(selectedCourse);
                 <div className="card-img">
                 <img className='photo' src={course.img} alt="" />
                 </div>
-                <h3>{course.course_title}</h3>
-                <p><small>
+                <h3 className='course-title'>{course.course_title}</h3>
+                <p className='details'><small>
                 {course.details}</small></p>
                 <div className="info">
-                    <p>Price: {course.price}</p>
-                    <p>Credit: {course.credit}</p>
+                    <p className='price'><AiOutlineStrikethrough/> Price: {course.price}</p>
+                    <p className='credit'><AiOutlineCalendar/> Credit: {course.credit}</p>
                 </div>
                 <button onClick={()=>handleClick(course)} className='card-btn'>Select</button>
         
